76e836b8a7a7fabb98a50ae468fc6acd9c8495b9
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-03-15 16:00:47 -0300

    feat: finalizado a montagem e estilização do carrinho de compras

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,7 +2,15 @@ import * as Dialog from "@radix-ui/react-dialog";
 import Image from "next/image";
 import { X } from "phosphor-react";
 import { CartButton } from "../CartButton";
-import { CartClose, CartContainer, CartProduct, CartProductDetails, CartProductImage } from "./styles";
+import {
+  CartClose,
+  CartContainer,
+  CartFinalization,
+  CartProduct,
+  CartProductDetails,
+  CartProductImage,
+  FinalizationDetails,
+} from "./styles";
 
 export function Cart() {
   return (
@@ -23,7 +31,7 @@ export function Cart() {
             {/* <p>Parece que seu carrinho esta vazio :(</p> */}
             <CartProduct>
               <CartProductImage>
-                <Image 
+                <Image
                   width={100}
                   height={93}
                   alt=""
@@ -31,12 +39,25 @@ export function Cart() {
                 />
               </CartProductImage>
               <CartProductDetails>
-                  <p>Produto 1</p>
-                  <strong>R$50,00</strong>
-                  <button>Remover</button>
+                <p>Produto 1</p>
+                <strong>R$50,00</strong>
+                <button>Remover</button>
               </CartProductDetails>
             </CartProduct>
           </section>
+          <CartFinalization>
+            <FinalizationDetails>
+              <div>
+                <span>Quantidade</span>
+                <p>2 itens</p>
+              </div>
+              <div>
+                <span>Valor total</span>
+                <p>R$100,00</p>
+              </div>
+            </FinalizationDetails>
+            <button>Finalizar Compra</button>
+          </CartFinalization>
         </CartContainer>
       </Dialog.Portal>
     </Dialog.Root>
