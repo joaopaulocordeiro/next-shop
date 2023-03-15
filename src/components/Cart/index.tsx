6f7ff5d27ae8ae11c3b2e8facdc1791a3bfa8bc2
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-03-15 12:40:46 -0300

    feat: criacao do dialog do carrinho de compras

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,5 +1,29 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'phosphor-react';
 import { CartButton } from "../CartButton";
+import { CartClose, CartContainer } from './styles';
 
 export function Cart() {
-  return <CartButton />;
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <CartButton />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <CartContainer>
+          <CartClose>
+            <X size={24} weight="bold"/>
+          </CartClose>
+
+
+          <h2>Sacola de Compras</h2>
+
+          <section>
+            <p>Parece que seu carrinho esta vazio :(</p>
+          </section>
+        </CartContainer>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
